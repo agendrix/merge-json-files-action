619b6b7fbf6e09defb009f8ca61b841edf10dbb0
Louis <39057274+LouisBoudreau@users.noreply.github.com>
2021-11-25 08:43:40 -0500

    Initial commit

---
+++ helpers/action/exec.ts
@@ -0,0 +1,21 @@
+import { exec as coreExec } from "@actions/exec";
+
+/**
+ * By default, the core exec() function returns the response number of the command.
+ * This returns the stdout data instead.
+ * This action fails if data is outputted to stderr
+ * @param commandLine The command to run
+ */
+export const exec = async (commandLine: string, failOnStdErr: boolean = true, silent: boolean = true) => {
+  let stdoutData = "";
+  await coreExec(commandLine.trim(), undefined, {
+    listeners: {
+      stdout: (data: Buffer) => {
+        stdoutData += data.toString();
+      },
+    },
+    failOnStdErr,
+    silent
+  });
+  return stdoutData;
+};
